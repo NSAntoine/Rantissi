3bf86b738b108a84af04160739f2437468e2e9ed
Serena <osama.id78@gmail.com>
2023-07-26 22:59:12 +0300

    Initial Commit

---
+++ Rantissi/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Rantissi
+//
+//  Created by Serena on 26/07/2023.
+//  
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
